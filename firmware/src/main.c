70b4a08d5353395ff2b861e25b4fa785657acb1a
Henryk Paluch <hpaluch@seznam.cz>
2019-11-10 16:46:47 +0100

    initial commit

---
+++ firmware/src/main.c
@@ -0,0 +1,67 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+// from: https://www.microchip.com/forums/m1108535.aspx
+/* This function is called after period expires */ 
+void TIMER1_EventHandler(uint32_t status, uintptr_t context)
+{
+    /* Toggle LED */
+    GPIO_RA0_Toggle();
+} 
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    TMR1_CallbackRegister(TIMER1_EventHandler,(uintptr_t)NULL); 
+    TMR1_Start();
+    
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
